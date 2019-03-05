85ce0d657020f261fe42eb54c64b1ef150025a77
Sergey <zhukovskysc1610@gmail.com>
2019-03-05 09:16:24 +0200

    Перезалив уроков на github

---
+++ Extended-Syntax/task.js
@@ -0,0 +1,83 @@
+"use strict"
+
+function calculateQuadraticEquation(){
+    let a = +window.a.value;
+    let b = +window.b.value;
+    let c = +window.c.value;
+    let result = getResult(a,b,c);
+    window.equation.textContent = `${a}*x^2 + (${b})*x + (${c}) = 0`;
+    let span = window.result;
+    span.textContent = "х = "+result;
+}
+
+function getResult(a,b,c){
+    // код для задачи №1 писать здесь
+	
+    let d = Math.pow(b,2) - 4 * a * c;
+    let x = [];
+	
+    if(d == 0) {
+	x = -b / (2 * a);
+	console.log(`Дискриминант = ${d}, корень один. x = ${x}`);
+    } else if(d > 0) {
+	x[0] = (-b - Math.sqrt(d)) / (2 * a);
+        x[1] = (-b + Math.sqrt(d)) / (2 * a);
+	console.log(`Дискриминант = ${d}, существует 2 решения уравнения. x1 = ${x[0]}, x2 = ${x[1]}`);
+    } else if(d < 0) {
+	console.log(`Дискриминант = ${d}, корней нету.`);
+    }
+	
+    return x;
+}
+
+function calculateDrinkTask(){
+    let name = window.personName.value;
+    let dateOfBirthday = new Date(window.dateOfBirthday.value);
+    let drink = askDrink(name, dateOfBirthday);
+    window.drink.textContent = drink;
+}
+
+function askDrink(name,dateOfBirthday){
+    // код для задачи №2 писать здесь
+	
+    let nowDate = new Date();
+
+    let year = nowDate.getFullYear();
+	
+    let age = year - dateOfBirthday.getFullYear();
+	
+    console.log(age);
+	
+    let result = (age < 18) ? `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!` : `Не желаете ли олд-фэшн, ${name}?`;
+	
+    console.log(result);
+	
+    return result;
+}
+
+function calculateAverageRating(){
+    let marks = window.marks.value.split("").map(Number);
+    let averageMark = getAverageMark(marks);
+    window.averageMark.textContent = averageMark;
+}
+
+function getAverageMark(marks){
+    // код для задачи №3 писать здесь
+	
+    let averageMark = 0;
+    let sumMarks = 0;
+    const numberOfValues = 5;
+	
+    if (marks.length > numberOfValues) {
+	    console.log(`Количество введенных оценок ${marks.length}!`);
+	    marks = marks.slice(0,numberOfValues);
+    }
+	
+    for(let i = 0; i < marks.length; i++) {
+            sumMarks += marks[i];
+    }
+	
+    averageMark = sumMarks / marks.length;
+	
+    return averageMark;
+}
---
+++ variables/main.js
@@ -0,0 +1,34 @@
+// Задача №1
+
+let x;
+let y;
+let z;
+
+x = 2;
+y = 22;
+z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
+
+// Задача №2
+
+let algebra;
+let geography;
+let physics;
+
+algebra = 5;
+geography = 4;
+physics = 4;
+
+let averageRating = (algebra + geography + physics) / 3;
+
+console.log(averageRating);
+
+// Задача №3
+
+let name = "Sergey";
+let message = `Привет, мир! Меня зовут ${name}`;
+
+console.log(message);

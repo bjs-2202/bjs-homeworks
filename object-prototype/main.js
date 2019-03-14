96f9286c41b69ddc5ffc5d27fa00b39531b77b2d
Sergey <47921614+bjs-2202@users.noreply.github.com>
2019-03-14 10:44:20 +0200

    refactoring

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -8,21 +8,21 @@ function initCheckBirthday() {
 
 function checkBirthday(birthday) {
     // код для задачи №1 писать здесь
-	let now = +new Date;
-	//console.log(now);
-	let dateOfBirth = +new Date(birthday);
-	//console.log(dateOfBirth);
-	let diff = now - dateOfBirth;
-	let age = diff/31471200000;
-	//console.log(age);
-	if (age >= 18) {
-	return age;
-	}
+    let now = +new Date;
+    //console.log(now);
+    let dateOfBirth = +new Date(birthday);
+    //console.log(dateOfBirth);
+    let diff = now - dateOfBirth;
+    let age = diff/31471200000;
+    //console.log(age);
+    if (age >= 18) {
+	    return age;
+    }
 }
 
 function initPrintAnimalSound() {
     const animal = {
-        sound: 'grrrr',
+	    sound: 'grrrr',
     };
 
     const result = getAnimalSound(animal);
@@ -32,31 +32,29 @@ function initPrintAnimalSound() {
 
 function getAnimalSound(animal) {
     // код для задачи №2 писать здесь
-	const sound = animal.sound;
+    const sound = animal.sound;
     if (animal == undefined){
-        return null
+	    return null
     } else {
-        return sound
+	    return sound
     }
 }
 
 function initCalculateStatement() {
     for (let idx = 0; idx < 3; idx++) {
-        const marks = document.getElementById('learner-' + idx).value.split(',');
-
-        const average = getAverageMark(marks);
-
-        document.getElementById('learner-' + idx + '-average').innerHTML = average;
+	    const marks = document.getElementById('learner-' + idx).value.split(',');
+	    const average = getAverageMark(marks);
+	    document.getElementById('learner-' + idx + '-average').innerHTML = average;
     }
 }
 
 function getAverageMark(marks) {
     // код для задачи №3 писать здесь
-	let average = 0;
-	for (let i in marks) {
-	//console.log(marks[i])
+    let average = 0;
+    for (let i in marks) {
+    //console.log(marks[i])
 	    average += marks[i];
-	}
-	let roundedAverage = average/marks.length;
-	return roundedAverage;
+    }
+    let roundedAverage = average/marks.length;
+    return roundedAverage;
 }
